4be458e3cbfb37223d775802b382480d25d98766
Nick Graffagna <n.graffagna@gmail.com>
2018-07-07 18:02:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ user_input.to_i-1
     if valid_move?(index) == true
       move(index,value)
         @board[index] = value
-      display_board  
+      display_board
     else
       turn
     end
